f43c04206d4f6c7b664a5020d69349aa8c2ef746
LBorgess <lucas_rancer@hotmail.com>
2023-10-29 15:11:50 -0300

    Criação e definição da conexão com o banco de dados

---
+++ app/Database.php
@@ -0,0 +1,46 @@
+<?php
+
+use \PDO;
+use \PDOException;
+
+/**
+ * Classe responsável por realizar a conexão e operações com o banco de dados
+ */
+class Database
+{
+    /**
+     * HOST de conexão com o banco de dados
+     * @var string
+     */
+    const HOST = 'localhost';
+
+    /**
+     * Nome do banco de dados
+     * @var string
+     */
+    const NAME = 'wishlist';
+
+    /**
+     * Usuário do banco de dados
+     * @var string
+     */
+    const USER = 'etecia';
+
+    /**
+     * Senha de acesso ao banco de dados
+     * @var string
+     */
+    const PASS = 'api@etec.com';
+
+    /**
+     * Nome da tabela que será manipulada
+     * @var string
+     */
+    private $table;
+
+    /**
+     * Instância de conexão com o banco de dados
+     * @var PDO
+     */
+    private $connection;
+}
