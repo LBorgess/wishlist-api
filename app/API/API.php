b161b32a66115156899feb02ee6208d3629dfd88
LBorgess <lucas_rancer@hotmail.com>
2023-11-01 21:40:35 -0300

    Inicio da classe API

--- app/API/API.php
+++ app/API/API.php
@@ -1,4 +1,33 @@
 <?php
 
 namespace App\API;
-/* API */
+
+use App\Database\Database;
+
+/**
+ * Classe responsável por realizar as operações da API
+ * @author LBorgess
+ */
+class API
+{
+    /**
+     * Váriavel para controlar o response.
+     * @var array
+     */
+    public $response = array();
+
+    /**
+     * Váriavel para avisar se está disponivel os parâmetros
+     * @var boolean
+     */
+    public $available = true;
+
+    /**
+     * Váriavel para informar o erro.
+     * @var string
+     */
+    public $missing = '';
+
+    
+
+}
