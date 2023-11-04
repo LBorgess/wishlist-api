85b53469f94e8b296f02776d749575abd5a10bc3
LBorgess <lucas_rancer@hotmail.com>
2023-11-04 12:31:01 -0300

    Criação dos demais métodos da API

--- app/API/API.php
+++ app/API/API.php
@@ -54,19 +54,107 @@ class API
         }
     }
 
-    public function apicall()
+    /**
+     * Método responsável por realizar a chamada de API
+     * @param string $method
+     * @return void
+     */
+    public function apicall($method)
     {
-    }
-    public function post()
-    {
-    }
-    public function put()
-    {
-    }
-    public function delete()
-    {
+        switch ($_GET[$method]) {
+            case 'post':
+                self::post();
+                break;
+            case 'put':
+                self::put();
+                break;
+            case 'delete':
+                self::delete();
+                break;
+            case 'get':
+                self::get();
+                break;
+            default:
+                $response['error'] = true;
+                $response['message'] = 'Chamada inválida.';
+                break;
+        }
     }
 
+    /**
+     * Método responsável por realizar a criação de um item na
+     * lista de desejos
+     * @return void
+     */
+    public function post()
+    {
+        self::verify(array('produto', 'link', 'preco', 'categoria'));
+
+        $obWishlist = new Wishlist();
+
+        $result = $obWishlist->cadastrar();
+
+        if ($result) {
+            $response['error'] = false;
+            $response['message'] = 'Produto cadastrado com sucesso.';
+            $response['wishlist'] = $obWishlist->getProdutos();
+        } else {
+            $response['error'] = true;
+            $response['message'] = 'Ocorreu um erro durante a criação.';
+        }
+    }
+
+    /**
+     * Método responsável por realizar a atualização da lista de
+     * desejos
+     * @return void
+     */
+    public function put()
+    {
+        self::verify(array('produto', 'link', 'preco', 'categoria'));
+
+        $obWishlist = new Wishlist();
+
+        $result = $obWishlist->atualizar();
+
+        if ($result) {
+            $response['error'] = false;
+            $response['message'] = 'Produto atualizado com sucesso.';
+            $response['wishlist'] = $obWishlist->getProdutos();
+        } else {
+            $response['error'] = true;
+            $response['message'] = 'Ocorreu um erro durante a atualização.';
+        }
+    }
+
+    /**
+     * Método responsável por realizar a exclusão de um produto
+     * da lista de desejos
+     * @return void
+     */
+    public function delete()
+    {
+        if (isset($_GET['id'])) {
+            $obWishlist = new Wishlist();
+            if ($obWishlist->excluir()) {
+                $response['error'] = false;
+                $response['message'] = 'Produto excluido com sucesso';
+                $response['wishlist'] = $obWishlist->getProdutos();
+            } else {
+                $response['error'] = true;
+                $response['message'] = 'Ocorreu algum erro durante a exclusão';
+            }
+        } else {
+            $response['error'] = true;
+            $response['message'] = 'Não foi possível excluir, forneça um ID.';
+        }
+    }
+
+    /**
+     * Método responsável por obter todos os itens da lista de
+     * desejos.
+     * @return void
+     */
     public function get()
     {
         $obWishlist = new Wishlist();
